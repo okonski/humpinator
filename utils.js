e18768c03c724b33c4a6e77c4bca247c95eb542d
Piotrek Oko?ski <piotrek@okonski.org>
2012-03-29 01:03:24 +0200

    Slight refactoring

--- utils.js
+++ utils.js
@@ -1,15 +1,15 @@
 var fitImages = function(list, max_width, buffer){
   list.each(function(i,image){
     var img = $(image);
+    var wrapper;
     if (img.width() + img.offset().left  >= max_width || (typeof img.data('originalWidth') != 'undefined' && img.data('originalWidth') + img.offset().right >= max_width)){
       if (!img.data('originalWidth')){
         img.attr('data-original-width',img.width());
         img.attr('data-original-height',img.height());
-      }
-      var wrapper = img.parent('a.humpinator-wrapper');
-      if (wrapper.length == 0){
         wrapper = img.wrap($("<a class='humpinator-wrapper'/>")).parent();
         wrapper.prepend($("<span class='humpinator-label'/>").text('Resized from ' + img.data('originalWidth') + 'x' + img.data('originalHeight')));
+      } else {
+        wrapper = img.parent('a.humpinator-wrapper');
       }
       img.css('width',max_width - img.offset().left - buffer);
       wrapper.attr('href',img.attr('src')).attr('target','_blank');
